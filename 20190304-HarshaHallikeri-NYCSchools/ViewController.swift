68551abef7f8ebfd3556f828bb76d68fb8d33350
harshahallikeri7 <harshahallikeri@harshas-mbp-2.lan>
2019-03-02 00:01:27 -0800

    Initial Commit

---
+++ 20190304-HarshaHallikeri-NYCSchools/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  20190304-HarshaHallikeri-NYCSchools
+//
+//  Created by Harsha Hallikeri on 3/2/19.
+//  Copyright © 2019 Harsha Hallikeri. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
